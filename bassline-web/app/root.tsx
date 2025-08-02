61974eda20243a7d1b7ba6f6fa414498e4461a7b
mercuricchloride <mercuryprotocol@protonmail.com>
2025-08-01 18:03:00 -0700

    fonts

--- bassline-web/app/root.tsx
+++ bassline-web/app/root.tsx
@@ -20,7 +20,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap",
   },
 ];
 
