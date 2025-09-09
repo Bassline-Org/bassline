5badb7eaff85528a57f620bdbcf1a201a80b03ce
mercuricchloride <mercuryprotocol@protonmail.com>
2025-09-09 15:02:15 -0700

    latest

--- port-graphs/src/pp/core.ts
+++ port-graphs/src/pp/core.ts
@@ -1,32 +1,149 @@
-export interface Gadget<TIn = unknown> {
-  receive(data: TIn): void;
-}
+import { defMulti, DispatchKey, MultiMethod } from "./multi";
+import _ from "lodash";
 
-export type Protocol<TIn, TApply = unknown, TConsider = unknown> = {
-  apply: (data: TIn) => TApply | null;
-  consider: (result: TApply) => TConsider | null;
-  act: (result: TConsider, gadget: Gadget<TIn>) => void;
-}
+const createCell = <Curr, Inc, Actions extends DispatchKey = BaseCellActions>(merge: CellMergeFn<Curr, Inc, Actions>, initial: Curr): Cell<Curr, Inc> => {
+  let current = initial;
 
-/**
- * Creates a protocol function
- * @param apply - The function to apply to the data
- * @param consider - The function to consider the result
- * @param act - The function to act on the result
- * @returns A protocol function
- */
-export function protocol<G extends Gadget<TIn>, TIn, TApply, TConsider>(
-  apply: (data: TIn) => TApply | null,
-  consider: (result: TApply) => TConsider | null,
-  act: (result: TConsider, gadget: G) => void
-): (this: G, data: TIn) => void {
-  return function(this: G, data: TIn): void {
-    const result = apply(data);
-    if (result == null) return;
-    
-    const decision = consider(result);
-    if (decision == null) return;
-    
-    act(decision, this);
+  const cell: Cell<Curr, Inc> = {
+    current: () => current,
+    emit: (_effect) => {},
+    receive: (data: Inc) => {
+      const result = merge(current, data);
+      if (!_.isEqual(result, current)) {
+        current = result;
+        cell.emit(['changed', result]);
+      }
+    },
   };
+  
+  return cell;
+};
+
+export const setUnion = defMulti((curr: any[], inc: any[]) => {
+  const [current, incoming] = [curr, inc].map(v => v ?? []);
+  if (_.isEqual(current, incoming)) return 'ignore';
+  return 'merge';
+})
+.defMethods({
+  'ignore': (curr, _inc) => curr,
+  'merge': (curr, inc) => _.union(curr, inc),
+});
+
+export const setIntersection = defMulti((curr: any[], inc: any[]) => {
+  const [current, incoming] = [curr, inc].map(v => v ?? []);
+  if (_.intersection(current, incoming).length === 0) return 'contradiction';
+  if (_.isEqual(current, incoming)) return 'ignore';
+  return 'merge';
+})
+.defMethods({
+  'ignore': (curr, _inc) => curr as any[],
+  'merge': (curr, inc) => _.intersection(curr, inc) as any[],
+  'contradiction': (curr, _inc) => curr as any[],
+});
+
+export const setDifference = defMulti((curr: any[], inc: any[]) => {
+  const [current, incoming] = [curr, inc].map(v => v ?? []);
+  if (_.isEqual(current, incoming)) return 'ignore';
+  return 'merge';
+})
+.defMethods({
+  'ignore': (curr, _inc) => curr,
+  'merge': (curr, inc) => _.difference(curr ?? [], inc ?? []),
+});
+
+const foo = createCell(setUnion, [1]);
+const bar = createCell(setUnion, []);
+const baz = createCell(setIntersection, [1,2,3,4,5]);
+const qux = createCell(setDifference, [1,2,3,4,5]);
+
+foo.emit = (data) => {
+  const [effect, ...args] = data;
+  switch (effect) {
+    case 'changed': {
+      const [result] = args;
+      console.log('foo: ', result);
+      bar.receive(result);
+      baz.receive(result);
+      break;
+    }
+    case 'noop': {
+      console.log('foo: noop');
+      break;
+    }
+  }
+};
+
+bar.emit = (data) => {
+  const [effect, ...args] = data;
+  switch (effect) {
+    case 'changed': {
+      const [result] = args;
+      console.log('bar: ', result);
+      qux.receive(result);
+      break;
+    }
+    case 'noop': {
+      console.log('bar: noop');
+      break;
+    }
+  }
+};
+
+baz.emit = (data) => {
+  const [effect, ...args] = data;
+  switch (effect) {
+    case 'changed': {
+      const [result] = args;
+      console.log('baz: ', baz.current());
+      console.log('baz: ', result);
+      break;
+    }
+    case 'noop': {
+      console.log('baz: ', baz.current());
+      console.log('baz: noop');
+      break;
+    }
+  }
+};
+
+qux.emit = (data) => {
+  const [effect, ...args] = data;
+  switch (effect) {
+    case 'changed': {
+      const [result] = args;
+      console.log('qux: ', qux.current());
+      console.log('qux: ', result);
+      break;
+    }
+  }
+};
+
+foo.receive([2]);
+foo.receive([3]);
+
+foo.receive([1]);
+foo.receive([2]);
+
+// ================================
+// Types
+// ================================
+
+export interface Gadget<TIn, Effect> {
+  receive: (data: TIn) => void;
+  emit: (data: Effect) => void;
+}
+export type CellMergeFn<Curr, Inc, Actions extends DispatchKey = BaseCellActions>
+= MultiMethod<[Curr, Inc], Actions, Curr>;
+
+export type BaseCellActions 
+= 'ignore' | 'merge';
+export type RefinementCellActions
+= 'contradiction' | BaseCellActions;
+
+export type CellEffect<Curr> =
+['changed', Curr] |
+['noop'];
+
+export interface Cell<Curr, Inc> extends Gadget<Inc, CellEffect<Curr>> {
+  current: () => Curr;
 }
--- port-graphs/src/pp/multi.ts
+++ port-graphs/src/pp/multi.ts
@@ -4,41 +4,28 @@
  * The dispatch function simply returns a key into the method map
  * And the dispatch functions and method implementations have the same input signature
  */
-
-export type DispatchFn<T extends any[] & {length: T['length']}, K> = (...args: T) => K;
-
-export type DispatchMap<K extends string | number | symbol, V, T extends any[] & {length: T['length']}> = 
-    Record<K, (...args: T) => V>
-
-export type MultiMethod<T extends any[] & {length: T['length']}, K extends string | number | symbol, V> = {
-    dispatch: DispatchFn<T, K>,
-    action: DispatchMap<K, V, T>,
-    defMethod: (key: K, fn: (...args: T) => V) => void,
-    defMethods: (methods: Record<K, (...args: T) => V>) => void,
-    (...args: T): V
-}
-
 export function defMulti<
     T extends any[] & {length: T['length']},
-    K extends string | number | symbol,
     V,
 >(
-    dispatch: DispatchFn<T, K>,
-): MultiMethod<T, K, V> {
-    const action = {} as DispatchMap<K, V, T>;
+    dispatch: DispatchFn<T, DispatchKey>,
+): MultiMethod<T, DispatchKey, V> {
+    const action = {} as DispatchMap<V, T>;
 
-    const defMethod = (key: K, fn: (...args: T) => V) => {
+    const defMethod = (key: DispatchKey, fn: (...args: T) => V) => {
         action[key] = fn;
+        return call;
     };
-    const defMethods = (methods: Record<K, (...args: T) => V>) => {
+    const defMethods = (methods: Record<string | number | symbol | 'default', (...args: T) => V>) => {
         for (const [key, fn] of Object.entries(methods)) {
             action[key] = fn;
         }
+        return call;
     };
 
     function call(...args: T): V {
         const key = dispatch(...args);
-        const fn = action[key] || action['default'];
+        const fn = action[key] || action['default' as string | number | symbol | 'default'];
         if (!fn) {
             throw new Error(`No method or default for key: ${key.toString()}`);
         }
@@ -52,3 +39,28 @@ export function defMulti<
 
     return call
 }
+
+// ================================
+// Types
+// ================================
+
+export type DispatchFn<T extends any[] & {length: T['length']}, K>
+    = (...args: T) => K;
+
+export type DispatchKey = string | number | symbol | 'default';
+export type DispatchMap<
+    Return,
+    T extends any[] & {length: T['length']}>
+        = Record<string |number | symbol | 'default', (...args: T) => Return>
+
+export type MultiMethod<
+    T extends any[] & {length: T['length']},
+    Key extends string | number | symbol, 
+    Return> 
+    = {
+        dispatch: DispatchFn<T, Key>,
+        action: DispatchMap<Return, T>,
+        defMethod: (key: Key, fn: (...args: T) => Return) => MultiMethod<T, Key, Return>,
+        defMethods: (methods: Record<Key, (...args: T) => Return>) => MultiMethod<T, Key, Return>,
+        (...args: T): Return
+    }
