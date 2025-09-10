f39b81667f5db2e0edc816007ac9ef88cf679a21
mercuricchloride <mercuryprotocol@protonmail.com>
2025-09-09 18:13:25 -0700

    latest

--- port-graphs/src/pp/core.ts
+++ port-graphs/src/pp/core.ts
@@ -1,60 +1,123 @@
-import { defMulti, DispatchKey, MultiMethod } from "./multi";
+import { defMulti } from "./multi";
 import _ from "lodash";
 
-const createCell = <Curr, Inc, Actions extends DispatchKey = BaseCellActions>(merge: CellMergeFn<Curr, Inc, Actions>, initial: Curr): Cell<Curr, Inc> => {
+export function noop() {
+  return ['noop'] as const;
+}
+export function changed<T>(value: T) {
+  return ['changed', value] as const;
+}
+export function contradiction<Curr, Inc>(cell: Cell<Curr, Inc, unknown>, current: Curr, incoming: Inc) {
+  return ['contradiction', cell, current, incoming] as const;
+}
+
+
+const createCell = <
+  TMerge extends (...args: any[]) => any
+>(
+  merge: TMerge,
+  initial: any
+): Cell<any, any, ReturnType<TMerge>> => {
   let current = initial;
 
-  const cell: Cell<Curr, Inc> = {
-    current: () => current,
+  const cell: Cell<any, any, ReturnType<TMerge>> = {
+    current,
+    // By default emit does nothing, this can be override based on the context
     emit: (_effect) => {},
-    receive: (data: Inc) => {
-      const result = merge(current, data);
-      if (!_.isEqual(result, current)) {
-        current = result;
-        cell.emit(['changed', result]);
+    receive: (data: any) => {
+      const effect = merge(cell, current, data);
+      if (effect !== undefined) {
+        cell.emit(effect);
       }
     },
   };
-  
+
   return cell;
 };
 
-export const setUnion = defMulti((curr: any[], inc: any[]) => {
-  const [current, incoming] = [curr, inc].map(v => v ?? []);
-  if (_.isEqual(current, incoming)) return 'ignore';
+export const setUnion = <T>() => defMulti((_cell: Cell<T[], T[], any>, current: T[], incoming: T[]) => {
+  const [curr, inc] = [current, incoming].map(v => v ?? []);
+  if (_.isEqual(curr, inc)) return 'ignore';
   return 'merge';
-})
-.defMethods({
-  'ignore': (curr, _inc) => curr,
-  'merge': (curr, inc) => _.union(curr, inc),
+}).defMethods({
+  'ignore': (_cell, _current, _incoming) => {
+    return noop();
+  },
+  'merge': (cell, current, incoming) => {
+    const result = _.union(current, incoming);
+    if (!_.isEqual(result, current)) {
+      cell.current = result;
+      return changed(result);
+    } else {
+      return noop();
+    }
+  },
 });
 
-export const setIntersection = defMulti((curr: any[], inc: any[]) => {
-  const [current, incoming] = [curr, inc].map(v => v ?? []);
-  if (_.intersection(current, incoming).length === 0) return 'contradiction';
-  if (_.isEqual(current, incoming)) return 'ignore';
+export const setIntersection = <T>() => defMulti((_cell: Cell<T[], T[], any>, current: T[], incoming: T[]) => {
+  const [curr, inc] = [current, incoming].map(v => v ?? []);
+  if (_.intersection(curr, inc).length === 0) return 'contradiction';
+  if (_.isEqual(curr, inc)) return 'ignore';
   return 'merge';
-})
-.defMethods({
-  'ignore': (curr, _inc) => curr as any[],
-  'merge': (curr, inc) => _.intersection(curr, inc) as any[],
-  'contradiction': (curr, _inc) => curr as any[],
+}).defMethods({
+  'ignore': (_cell, _current, _incoming) => {
+    return noop();
+  },
+  'merge': (cell, current, incoming) => {
+    const result = _.intersection(current, incoming);
+    if (!_.isEqual(result, current)) {
+      cell.current = result;
+      return changed(result);
+    } else {
+      return noop();
+    }
+  },
+  'contradiction': (cell, _current, _incoming) => {
+    return contradiction(cell, _current, _incoming);
+  },
 });
 
-export const setDifference = defMulti((curr: any[], inc: any[]) => {
-  const [current, incoming] = [curr, inc].map(v => v ?? []);
-  if (_.isEqual(current, incoming)) return 'ignore';
+export const setDifference = <T>() => defMulti((_cell: Cell<T[], T[], any>, current: T[], incoming: T[]) => {
+  const [curr, inc] = [current, incoming].map(v => v ?? []);
+  if (_.isEqual(curr, inc)) return 'ignore';
   return 'merge';
-})
-.defMethods({
-  'ignore': (curr, _inc) => curr,
-  'merge': (curr, inc) => _.difference(curr ?? [], inc ?? []),
+}).defMethods({
+  'ignore': (_cell, _current, _incoming) => {
+    return noop();
+  },
+  'merge': (cell, current, incoming) => {
+    const result = _.difference(current ?? [], incoming ?? []);
+    if (!_.isEqual(result, current)) {
+      cell.current = result;
+      return changed(result);
+    } else {
+      return noop();
+    }
+  },
 });
 
-const foo = createCell(setUnion, [1]);
-const bar = createCell(setUnion, []);
-const baz = createCell(setIntersection, [1,2,3,4,5]);
-const qux = createCell(setDifference, [1,2,3,4,5]);
+export const max = defMulti((_cell: Cell<number, number, any>, current: number, incoming: number) => {
+  if (current > incoming) return 'ignore';
+  return 'max';
+}).defMethods({
+  'ignore': (_cell, _current, _incoming) => {
+    return noop();
+  },
+  'max': (cell, current, incoming) => {
+    const result = Math.max(current, incoming);
+    if (!_.isEqual(result, current)) {
+      cell.current = result;
+      return changed(result);
+    } else {
+      return noop();
+    }
+  },
+});
+
+const foo = createCell(setUnion<number>(), [1]);
+const bar = createCell(setUnion<number>(), [] as number[]);
+const baz = createCell(setIntersection<number>(), [1,2,3,4,5]);
+const qux = createCell(setDifference<number>(), [1,2,3,4,5]);
 
 foo.emit = (data) => {
   const [effect, ...args] = data;
@@ -62,8 +125,8 @@ foo.emit = (data) => {
     case 'changed': {
       const [result] = args;
       console.log('foo: ', result);
-      bar.receive(result);
-      baz.receive(result);
+      bar.receive(result ?? []);
+      baz.receive(result ?? []);
       break;
     }
     case 'noop': {
@@ -79,7 +142,7 @@ bar.emit = (data) => {
     case 'changed': {
       const [result] = args;
       console.log('bar: ', result);
-      qux.receive(result);
+      qux.receive(result ?? []);
       break;
     }
     case 'noop': {
@@ -94,12 +157,12 @@ baz.emit = (data) => {
   switch (effect) {
     case 'changed': {
       const [result] = args;
-      console.log('baz: ', baz.current());
+      console.log('baz: ', baz.current);
       console.log('baz: ', result);
       break;
     }
     case 'noop': {
-      console.log('baz: ', baz.current());
+      console.log('baz: ', baz.current);
       console.log('baz: noop');
       break;
     }
@@ -111,7 +174,7 @@ qux.emit = (data) => {
   switch (effect) {
     case 'changed': {
       const [result] = args;
-      console.log('qux: ', qux.current());
+      console.log('qux: ', qux.current);
       console.log('qux: ', result);
       break;
     }
@@ -132,18 +195,10 @@ export interface Gadget<TIn, Effect> {
   receive: (data: TIn) => void;
   emit: (data: Effect) => void;
 }
-export type CellMergeFn<Curr, Inc, Actions extends DispatchKey = BaseCellActions>
-= MultiMethod<[Curr, Inc], Actions, Curr>;
 
-export type BaseCellActions 
-= 'ignore' | 'merge';
-export type RefinementCellActions
-= 'contradiction' | BaseCellActions;
+export type CellMergeFn<Curr, Inc, TEffects = unknown> = (cell: Cell<Curr, Inc, TEffects>, current: Curr, incoming: Inc) => TEffects | undefined;
 
-export type CellEffect<Curr> =
-['changed', Curr] |
-['noop'];
 
-export interface Cell<Curr, Inc> extends Gadget<Inc, CellEffect<Curr>> {
-  current: () => Curr;
+export interface Cell<Curr, Inc, TEffects = unknown> extends Gadget<Inc, TEffects> {
+  current: Curr;
 }
--- port-graphs/src/pp/multi.ts
+++ port-graphs/src/pp/multi.ts
@@ -1,66 +1,38 @@
-/**
- * Clojure style multimethod dispatch
- * We define a dispatch function and a map of methods
- * The dispatch function simply returns a key into the method map
- * And the dispatch functions and method implementations have the same input signature
- */
-export function defMulti<
-    T extends any[] & {length: T['length']},
-    V,
->(
-    dispatch: DispatchFn<T, DispatchKey>,
-): MultiMethod<T, DispatchKey, V> {
-    const action = {} as DispatchMap<V, T>;
 
-    const defMethod = (key: DispatchKey, fn: (...args: T) => V) => {
-        action[key] = fn;
-        return call;
-    };
-    const defMethods = (methods: Record<string | number | symbol | 'default', (...args: T) => V>) => {
-        for (const [key, fn] of Object.entries(methods)) {
+export function defMulti<TArgs extends any[], TReturn extends string | number | symbol>(
+    dispatch: (...args: TArgs) => TReturn,
+) {
+    type BodyFn = (...args: TArgs & {length: TArgs['length']}) => any;
+    const action = {} as Record<TReturn, BodyFn>;
+
+    const defMethods = <TEffects extends Record<TReturn, BodyFn>>(
+        methods: TEffects
+    ) => {
+        for (const [key, fn] of Object.entries(methods) as [TReturn, BodyFn][]) {
             action[key] = fn;
         }
+        
+        function call(...args: TArgs): ReturnType<TEffects[keyof TEffects]> {
+            const key = dispatch(...args);
+            const fn = action[key] || action['default' as TReturn];
+            if (!fn) {
+                throw new Error(`No method or default for key: ${key.toString()}`);
+            }
+            return fn(...args) as ReturnType<TEffects[keyof TEffects]>;
+        }
+        
         return call;
     };
 
-    function call(...args: T): V {
+    function call(...args: TArgs): ReturnType<BodyFn> {
         const key = dispatch(...args);
-        const fn = action[key] || action['default' as string | number | symbol | 'default'];
+        const fn = action[key] || action['default' as TReturn];
         if (!fn) {
             throw new Error(`No method or default for key: ${key.toString()}`);
         }
         return fn(...args);
     };
 
-    call.dispatch = dispatch;
-    call.action = action;
-    call.defMethod = defMethod;
     call.defMethods = defMethods;
-
-    return call
+    return call;
 }
-
-// ================================
-// Types
-// ================================
-
-export type DispatchFn<T extends any[] & {length: T['length']}, K>
-    = (...args: T) => K;
-
-export type DispatchKey = string | number | symbol | 'default';
-export type DispatchMap<
-    Return,
-    T extends any[] & {length: T['length']}>
-        = Record<string |number | symbol | 'default', (...args: T) => Return>
-
-export type MultiMethod<
-    T extends any[] & {length: T['length']},
-    Key extends string | number | symbol, 
-    Return> 
-    = {
-        dispatch: DispatchFn<T, Key>,
-        action: DispatchMap<Return, T>,
-        defMethod: (key: Key, fn: (...args: T) => Return) => MultiMethod<T, Key, Return>,
-        defMethods: (methods: Record<Key, (...args: T) => Return>) => MultiMethod<T, Key, Return>,
-        (...args: T): Return
-    }
