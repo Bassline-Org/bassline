7ceaba0976d7b6d72e93ecef022d4a454af7b196
mercuricchloride <mercuryprotocol@protonmail.com>
2025-10-02 03:13:37 -0700

    removed examples

--- port-graphs-react/src/examples/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Examples - Showcasing port-graphs-react hooks
- */
-
-export { Counter } from './Counter';
-export { DerivedSum } from './DerivedSum';
-export { SyncedInputs } from './SyncedInputs';
-export { SharedState } from './SharedState';
-export { Pipeline } from './Pipeline';
