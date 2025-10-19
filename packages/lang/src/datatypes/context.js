ad4a9200fe37b82df75284d4bc441c82e7d9e3c7
mercuricchloride <mercuryprotocol@protonmail.com>
2025-10-18 20:41:06 -0700

    Playing with more prinicipled contexts

--- packages/lang/src/datatypes/context.js
+++ packages/lang/src/datatypes/context.js
@@ -1,5 +1,118 @@
 import { normalize, normalizeString } from "../utils.js";
-import { Datatype, nil, Str, Value, Word } from "./core.js";
+import {
+    Block,
+    Bool,
+    Datatype,
+    LitWord,
+    nil,
+    Str,
+    Value,
+    Word,
+} from "./core.js";
+
+export class ContextBase extends Value {
+    static type = normalizeString("any-context!");
+    constructor() {
+        super();
+        this.bindings = new Map();
+        this.set("self", this);
+    }
+
+    keyFor(word) {
+        if (typeof word === "symbol") return word;
+        if (word?.spelling) return word.spelling;
+        if (word instanceof Str) return normalize(word.value);
+        if (typeof word === "string") return normalize(word);
+        console.log("type: ", typeof word);
+        throw new Error(`Invalid string ${JSON.stringify(word)}`);
+    }
+
+    has(word) {
+        const spelling = this.keyFor(word);
+        const result = this.bindings.has(spelling);
+        return new Bool(result);
+    }
+
+    get(word) {
+        const spelling = this.keyFor(word);
+        const value = this.bindings.get(spelling);
+        if (value === undefined) {
+            return nil;
+        }
+        return value;
+    }
+
+    set(word, value) {
+        const spelling = this.keyFor(word);
+        this.bindings.set(spelling, value);
+        return value;
+    }
+
+    words() {
+        const allWords = [];
+        for (const spelling of this.bindings.keys()) {
+            allWords.push(new Word(spelling));
+        }
+        return new Block(allWords);
+    }
+
+    copy(targetContext) {
+        for (const [word, value] of this.bindings.entries()) {
+            if (word === this.keyFor("self")) continue;
+            console.log("word: ", word);
+            console.log("value: ", value);
+            targetContext.set(word, value);
+        }
+    }
+
+    project(words) {
+        const newContext = new this.constructor();
+        for (const word of words.items) {
+            if (word === this.keyFor("self")) continue;
+            newContext.set(word, this.get(word));
+        }
+        return newContext;
+    }
+
+    merge(contexts) {
+        const newContext = new this.constructor();
+        this.copy(newContext);
+        for (const ctx of contexts.items) {
+            ctx.copy(newContext);
+        }
+        return newContext;
+    }
+}
+
+export class ContextChain extends ContextBase {
+    static type = normalize("context!");
+    constructor(parent = nil) {
+        super();
+        if (parent !== nil) {
+            this.set("parent", parent);
+        }
+    }
+
+    hasParent() {
+        return this.bindings.has(this.keyFor("parent"));
+    }
+
+    parent() {
+        if (this.has("parent")) {
+            return this.get("parent");
+        }
+        return nil;
+    }
+
+    get(word) {
+        const key = this.keyFor(word);
+        if (this.bindings.has(key)) return this.bindings.get(key);
+        if (this.hasParent()) {
+            return this.parent().get(word);
+        }
+        return nil;
+    }
+}
 
 export class Context extends Value {
     static type = normalizeString("context!");
@@ -106,3 +219,38 @@ export class Context extends Value {
 export default {
     "context!": new Datatype(Context),
 };
+
+export function setMany(ctx, obj) {
+    for (const [key, value] of Object.entries(obj)) {
+        ctx.set(key, value);
+    }
+}
+
+const parent = new ContextBase();
+
+const a = new ContextChain(parent);
+setMany(a, {
+    a: { value: 123 },
+    b: { value: 456 },
+});
+
+const b = new ContextChain(parent);
+setMany(b, {
+    b: { value: 123 },
+    c: { value: 123123 },
+});
+
+const merged = a.merge(new Block([b]));
+
+const parentRef = merged.get("parent");
+parentRef.set("foo", { value: 123 });
+console.log(a.get("foo"));
+console.log(b.get("foo"));
+
+const projected = merged.project(
+    new Block(
+        ["a", "b"].map((e) => new Word(e)),
+    ),
+);
+
+console.log(projected.get("foo"));
