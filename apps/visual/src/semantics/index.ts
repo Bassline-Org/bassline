48c42fd1b55cf0b32133008c9e51e5ca51a34e7d
mercuricchloride <mercuryprotocol@protonmail.com>
2026-01-07 19:09:16 -0800

    more semantics

--- apps/visual/src/semantics/index.ts
+++ apps/visual/src/semantics/index.ts
@@ -7,6 +7,9 @@
 import { registerSemantic } from '../lib/semantics'
 import { JsonExportSemantic } from './json-export'
 import { FilterSemantic } from './filter'
+import { MergeSemantic } from './merge'
+import { MapSemantic } from './map'
+import { TemplateSemantic } from './template'
 
 // Register all semantics
 registerSemantic({
@@ -21,4 +24,25 @@ registerSemantic({
   name: 'Filter',
   icon: 'filter',
   component: FilterSemantic,
+})
+
+registerSemantic({
+  id: 'merge',
+  name: 'Merge',
+  icon: 'git-merge',
+  component: MergeSemantic,
+})
+
+registerSemantic({
+  id: 'map',
+  name: 'Map',
+  icon: 'wand',
+  component: MapSemantic,
+})
+
+registerSemantic({
+  id: 'template',
+  name: 'Template',
+  icon: 'file-code',
+  component: TemplateSemantic,
 })
