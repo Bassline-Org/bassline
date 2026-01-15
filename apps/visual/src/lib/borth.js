1acd0923a2aa178e70dc5a2570e4ae86f3d31dad
mercuricchloride <mercuryprotocol@protonmail.com>
2026-01-14 19:16:57 -0800

    a few little changes for borth

--- apps/visual/src/lib/borth.js
+++ apps/visual/src/lib/borth.js
@@ -1,276 +1,329 @@
-// borth
-// forth like
-// but for bassline
-const WS = ' \t\n\r'
+// borth - forth-like for bassline
 
-const word = {
-  _system: {},
-  get system() {
-    return word._system
-  },
-  ds: [],
-  get height() {
-    return this.ds.length
-  },
-  latest: undefined,
-  _make(obj = {}) {
-    const { name } = obj
-    let w
-    if (name && word.get(name)) {
-      w = word.system[name]
-    } else {
-      w = Object.create(this)
-      if (name) word._system[name] = w
-    }
-    Object.assign(w, obj)
-    return w
-  },
-  get(name) {
-    return this.system[name]
-  },
-  set(name, w) {
-    if (!w.prototype.isPrototypeOf(word)) panic(`invalid word stored: ${w}`)
-    const existing = word.get(name)
-    if (existing) {
-      Object.assign(existing, w)
-    } else {
-      this.system[name] = w
-    }
-  },
-  run() {
-    return this[word._system.mode.data]()
-  },
-  interp() {
-    word.ds.push(this)
-    //panic('no interp() method')
-  },
-  compile() {
-    if (this.immediate) return this.interp()
-    word.latest.data.push(this) ?? this.panic('failed to find latest')
-  },
+const WS = ' \t\n\r'
+const isWS = c => WS.includes(c)
+const derive = (base, over) => Object.assign(Object.create(base), over)
+const castArr = v => {
+  if ((v ?? undefined) === undefined) return []
+  if (!Array.isArray(v)) return [v]
+  return v
 }
 
-const variable = word._make({
-  make(name, data = 0) {
-    return this._make({ name, data })
+const stack = {
+  data: [],
+  create() {
+    const b = Object.create(this)
+    b.data = []
+    return b
   },
-  interp() {
-    word.ds.push(this.name)
-  },
-  get(name) {
-    return word.get(name).data
-  },
-  set(name, value) {
-    const w = word.get(name)
-    if (w) {
-      w.data = value
-    } else {
-      variable.make(name, value)
-    }
-  },
-  update(name, fn, ifAbsent) {
-    const w = word.get(name)
-    if (w.data === undefined || w.data === null) return this.set(name, ifAbsent)
-    return this.set(name, fn(w.data))
-  },
-})
-
-const stack = word._make({
-  make(name) {
-    return this._make({ name, data: [] })
-  },
-  castArr(val) {
-    if (Array.isArray(val)) return val
-    return [val]
-  },
-  push(val) {
-    return this.data.push(val)
-  },
-  pop() {
+  read() {
+    if (!this.data.length) throw new Error('stack underflow')
     return this.data.pop()
   },
-  shift() {
-    return this.data.shift()
+  write(v) {
+    this.data.push(v)
   },
-  unshift(val) {
-    return this.data.unshift(...this.castArr(val))
-  },
-  splice(start, del, val) {
-    return this.data.splice(start, del, ...this.castArr(val))
-  },
-  interp() {
-    word.ds.push(this)
-  },
-})
+}
 
-const compiled = word._make({
-  make(name) {
-    const w = this._make({ name, data: [] })
-    word.latest = w
+const buffer = {
+  data: [],
+  create(ring) {
+    const b = Object.create(this)
+    b.data = []
+    b.p = 0
+    b.ring = ring
+    return b
+  },
+  get size() {
+    return this.data.length
+  },
+  read() {
+    return this.data[this.p]
+  },
+  write(v) {
+    this.insert(v)
+  },
+  move(n) {
+    const s = this.size
+    this.p = this.ring && s ? (((this.p + n) % s) + s) % s : Math.max(0, Math.min(this.p + n, s))
+    return this.p
+  },
+  insert(...v) {
+    this.data.splice(this.p + 1, 0, ...v)
+    this.p += v.length
+  },
+  delete(n = 1) {
+    this.data.splice(this.p, n)
+  },
+  leap(f, dir = 1) {
+    for (let i = this.p + dir; dir > 0 ? i < this.size : i >= 0; i += dir)
+      if (f(this.data[i])) {
+        const s = this.p
+        this.p = i
+        return dir > 0 ? [s, i] : [i, s]
+      }
+    return []
+  },
+}
+
+const word = {
+  make(rt, props) {
+    const w = Object.create(this)
+    if (rt) w.rt = rt
+    w.attributes = {}
+    Object.assign(w, props)
+    if (rt && w.name) rt.dict[w.name] = rt.last = w
     return w
   },
+  attributes: {},
   interp() {
-    for (const w of this.data) {
-      w.run()
-    }
+    this.rt.ds.write(this)
   },
-})
-
-const lit = word._make({
-  make(val) {
-    if (val === undefined) panic('cannot define undefined lit')
-    return this._make({ data: val })
+  compile() {
+    this.immediate ? this.interp() : this.rt.target.write(this)
   },
-  interp() {
-    word.ds.push(this.data)
+  run() {
+    this[this.rt.mode]()
   },
-})
-
-const fn = word._make({
-  make(name, fn) {
-    const arity = fn.length
-    return this._make({ name, arity, fn })
+  read() {
+    return this.data
   },
-  interp() {
-    if (word.height < this.arity) panic('stack underflow')
-    const args = []
-    for (let i = 0; i < this.arity; i++) {
-      args.unshift(word.ds.pop())
-    }
-    let result = this.fn(...args) ?? []
-    if (!Array.isArray(result)) result = [result]
-    for (const res of result) {
-      if (res === undefined || res === null) continue
-      word.ds.push(res)
-    }
+  write(v) {
+    this.data = v
   },
-})
-
-const ifn = fn._make({
-  make(name, fn) {
-    const arity = fn.length
-    return this._make({ name, arity, fn, immediate: true })
-  },
-})
-
-// stack manipulation
-fn.make('dup', a => [a, structuredClone(a)])
-fn.make('drop', a => [])
-fn.make('swap', (a, b) => [b, a])
-fn.make('rot', (a, b, c) => [b, c, a])
-fn.make('over', (a, b) => [a, b, structuredClone(a)])
-fn.make('.', a => (console.log(a), []))
-// arithmetic
-fn.make('+', (a, b) => [a + b])
-fn.make('-', (a, b) => [a - b])
-fn.make('*', (a, b) => [a * b])
-fn.make('/', (a, b) => [a / b])
-// vars
-fn.make('variable', () => {
-  const name = parse(WS)
-  if (variable.get(name)) {
-    return
-  }
-  variable.set(name, 0)
-})
-fn.make('stack', () => {
-  const name = parse(WS)
-  stack.make(name)
-})
-fn.make('@', name => [variable.get(name)])
-fn.make('!', (val, name) => [variable.set(name, val)])
-// parsing words
-fn.make('parse', () => {
-  const chars = parse(WS)
-  return [parse(chars)]
-})
-fn.make("'", () => {
-  const w = parse(WS)
-  return [w]
-})
-fn.make('"', () => {
-  const str = parse('"')
-  return [str]
-})
-// compilation words
-fn.make(':', () => {
-  const name = parse(WS)
-  variable.set('mode', 'compile')
-  compiled.make(name)
-  return []
-})
-ifn.make(';', () => (variable.set('mode', 'interp'), []))
-fn.make('immediate', () => ((word.latest.immediate = true), []))
-fn.make('regular', () => ((word.latest.immediate = undefined), []))
-fn.make('dsp', () => console.log(word.ds))
-fn.make('>.', (value, loc) => (loc.unshift(value), []))
-fn.make('.>', (value, loc) => (loc.push(value), []))
-fn.make('>@', (value, index, loc) => (loc.splice(index, 0, value), []))
-fn.make('<.', loc => [loc.shift()])
-fn.make('.<', loc => [loc.pop()])
-fn.make('<@', (loc, index) => [loc.splice(index, 1)])
-fn.make('iota', n => {
-  const out = []
-  for (let i = 0; i < n; i++) {
-    out.push(i + 1)
-  }
-  return [out]
-})
-
-variable.make('inputBuffer', '')
-variable.make('mode', 'interp')
-
-function parse(delims) {
-  const input = variable.get('inputBuffer')
-  let i = 0
-  while (i < input.length && delims.includes(input[i])) i++
-  const start = i
-  while (i < input.length && !delims.includes(input[i])) i++
-  const stop = i
-  const parsed = input.slice(start, stop)
-  variable.set('inputBuffer', input.slice(stop + 1))
-  return stop > start ? parsed : undefined
 }
 
-function run(script) {
-  variable.update('inputBuffer', old => old.concat(script))
-  let w = parse(WS)
-  while (w) {
-    let entry = word.get(w)
-    if (!entry) {
-      const num = Number(w)
-      if (isNaN(num)) {
-        console.log(w)
-        panic(`unknown word: ${w}`)
+// Specialized prototypes
+const fn = derive(word, {
+  interp() {
+    const a = []
+    for (let i = 0; i < this.fn.length; i++) a.unshift(this.rt.ds.read())
+    const r = castArr(this.fn(...a))
+    for (const v of r) if (v !== null) this.rt.ds.write(v)
+  },
+})
+
+const lit = derive(word, {
+  interp() {
+    this.rt.ds.write(this.data)
+  },
+})
+
+const seq = derive(word, {
+  interp() {
+    for (const w of this.body) {
+      try {
+        w.interp()
+      } catch (e) {
+        if (e.exit) {
+          console.log('breaking')
+          break
+        }
+        throw e
       }
-      entry = lit.make(num)
     }
-    entry.run()
-    w = parse(WS)
+  },
+  write(w) {
+    this.body.push(w)
+  },
+})
+
+const stream = derive(word, {
+  interp() {
+    this.rt.ds.write(this.stream)
+  },
+  read() {
+    return this.stream.read()
+  },
+  write(v) {
+    this.stream.write(v)
+  },
+})
+
+// Runtime
+export function createRuntime() {
+  const rt = {
+    dict: {},
+    last: null,
+    mode: 'interp',
+    input: buffer.create(),
+    ds: stack.create(),
+    targets: [],
+    get target() {
+      return this.targets[this.targets.length - 1]
+    },
+    popTarget() {
+      return this.targets.pop()
+    },
+    pushTarget(t) {
+      return this.targets.push(t)
+    },
+    parse(d) {
+      const i = this.input
+      while (i.p < i.size && d(i.read())) i.move(1)
+      if (i.p >= i.size) return
+      const s = i.p
+      while (i.p < i.size && !d(i.read())) i.move(1)
+      i.move(1)
+      return i.data.slice(s, i.p - 1).join('')
+    },
+
+    next() {
+      const t = this.parse(isWS)
+      if (!t) return
+      let w = this.dict[t]
+      if (!w) {
+        const num = Number(t)
+        if (isNaN(num)) {
+          throw Error(`unknown: ${t}`)
+        }
+        w = lit.make(this, { data: +t })
+      }
+      return w
+    },
+
+    run(src) {
+      this.input.data = src.split('')
+      this.input.p = 0
+      let w = this.next()
+      while (w !== undefined) {
+        w.run()
+        w = this.next()
+      }
+    },
   }
+
+  const def = (n, f, imm) => fn.make(rt, { name: n, fn: f, immediate: imm })
+
+  // Stream
+  def('.>', (v, s) => {
+    s.write(v)
+  })
+  def('>.', s => [s.read()])
+  def('..>', (_a, s) => {
+    const a = castArr(_a)
+    for (const v of a) s.write(v)
+  })
+  def('>..', (s, n) => {
+    const o = []
+    for (let i = 0; i < n; i++) o.push(s.read())
+    return [o]
+  })
+  def('ds', () => rt.ds)
+  def('take', n => {
+    const o = []
+    for (let i = 0; i < n; i++) o.unshift(rt.ds.read())
+    return [o]
+  })
+  def('take-to', stop => {
+    const o = []
+    let val
+    while (rt.ds.data.length && (val = rt.ds.read())) {
+      if (val === stop) break
+      o.unshift(val)
+    }
+    return [o]
+  })
+  def('splice', arr => castArr(arr))
+  def('exit', () => {
+    const e = new Error('exiting')
+    e.exit = true
+    throw e
+  })
+  def('err', msg => {
+    throw new Error(`err: ${msg}`)
+  })
+
+  // Stack
+  def('dup', a => [a, a])
+  def('drop', _a => {})
+  def('swap', (a, b) => [b, a])
+  def('rot', (a, b, c) => [b, c, a])
+  def('over', (a, b) => [a, b, a])
+  def('.', a => console.log(a))
+
+  // Arithmetic
+  def('+', (a, b) => a + b)
+  def('-', (a, b) => a - b)
+  def('*', (a, b) => a * b)
+  def('/', (a, b) => a / b)
+
+  // Definitions
+  def('variable', () => {
+    word.make(rt, { name: rt.parse(isWS), data: 0 })
+  })
+  def('buffer', () => {
+    stream.make(rt, { name: rt.parse(isWS), stream: buffer.create() })
+  })
+  def('stack', () => {
+    stream.make(rt, { name: rt.parse(isWS), stream: stack.create() })
+  })
+  def(':', () => {
+    rt.mode = 'compile'
+    rt.pushTarget(seq.make(rt, { name: rt.parse(isWS), body: [] }))
+  })
+  def(
+    ';',
+    () => {
+      rt.mode = 'interp'
+      rt.popTarget()
+    },
+    true
+  )
+  def('next', () => {
+    const w = rt.next()
+    w.run()
+  })
+  def('immediate', () => {
+    if (rt.last) rt.last.immediate = true
+  })
+  def('>attr', (key, val) => {
+    if (rt.last) rt.last.attributes[key] = val
+  })
+  def('attr>', (name, attr) => {
+    return [rt.dict[name].attributes[attr]]
+  })
+  // Parsing
+  def('parse', delim => {
+    if (typeof delim !== 'string') throw new Error('invalid parse')
+    const str = rt.parse(c => delim.includes(c))
+    return [str]
+  })
+  def('parse-word', () => {
+    return [rt.parse(isWS)]
+  })
+  def(
+    "'",
+    () => {
+      const str = rt.parse(isWS)
+      if (rt.mode === 'compile') {
+        rt.target.write(lit.make(rt, { data: str }))
+      } else {
+        rt.ds.write(str)
+      }
+    },
+    true
+  )
+  def(
+    '"',
+    () => {
+      const str = rt.parse(c => c === '"')
+      if (rt.mode === 'compile') {
+        rt.target.write(lit.make(rt, { data: str }))
+      } else {
+        rt.ds.write(str)
+      }
+    },
+    true
+  )
+
+  def('iota', n => {
+    const o = []
+    for (let i = 1; i <= n; i++) o.push(i)
+    return [o]
+  })
+
+  return rt
 }
 
-function panic(msg) {
-  throw new Error(`panic: ${msg}`)
-}
-
-export { word, variable, compiled, fn, ifn, panic, run }
-
-const program = `
-
-stack foo
-
-: <foo foo .< ;
-: >foo foo >. ;
-
-20 >foo
-
-40 >foo
-
-<foo <foo + .
-
-100 iota >foo
-
-`
-
-run(program)
+export { word, fn, lit, seq, stream, buffer }
