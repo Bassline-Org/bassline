6c3b07170258de38127a0ded28dd9e90d4b2eb2a
mercuricchloride <mercuryprotocol@protonmail.com>
2026-05-12 22:54:26 -0700

    basic menu stub

---
+++ packages/std/src/menu.js
@@ -0,0 +1,38 @@
+import { msg, is, failure } from '@bassline/core'
+import { lambda } from './lambda.js'
+
+const description = `\
+I am a menu.
+I represent a group of logically related "callable" things.
+In other words I present a domain specific way of invoking "call" \
+on a group of lambdas.
+
+Each of my capabilities expects something with 'resolve' and 'reject'.
+
+Like lambda, I will invoke one of those always if present, ignoring
+the argument otherwise.`
+
+export function menu(verbs) {
+  if (!is.object(verbs))
+    throw failure(
+      `invalid verbs: must be an object. Got: ${JSON.stringify(verbs)}`
+    )
+
+  const m = msg({ description })
+
+  const caps = {}
+  for (const [verb, lam] of Object.entries(verbs)) {
+    let verbMsg
+    if (is.fn(lam)) {
+      verbMsg = lambda(lam)
+      verbMsg.closedBy(m)
+    } else if (is.msg(lam) && lam.capableOf('call')) {
+      verbMsg = lam
+    } else {
+      throw failure(`invalid verb: ${JSON.stringify(lam)}`)
+    }
+    caps[verb] = aMsg => verbMsg.invoke('call', aMsg)
+  }
+
+  return m.grantCaps(caps)
+}
---
+++ packages/std/test/menu.test.js
@@ -0,0 +1,51 @@
+import { describe, it, expect } from 'vitest'
+import { menu } from '../src/menu.js'
+import { AssertionFailure, msg, Msg, failure } from '@bassline/core'
+import { createPromise, lambda } from '../src/lambda.js'
+
+describe('smoke', () => {
+  it('should create a menu', () => {
+    const m = menu({})
+    expect(m).toBeInstanceOf(Msg)
+  })
+})
+
+describe('creating with functions', () => {
+  const m = menu({
+    foo: msg,
+    bar: () => msg({ scalar: 123 }),
+    oops: () => {
+      throw failure('oops')
+    },
+  })
+
+  it('should allow creation with functions', () => {
+    expect(m.capableOf(['foo', 'bar'])).toBe(true)
+  })
+
+  it('should allow "calling" with the verbs', async () => {
+    const [req, promise] = createPromise(msg({ hello: 'world' }))
+    m.invoke('bar', req)
+    await expect(promise).resolves.toBeInstanceOf(Msg)
+    const res = await promise
+    expect(res.get('scalar')).toBe(123)
+  })
+
+  it('should error when not providing functions', () => {
+    expect(() => menu({ foo: 123 })).toThrow(AssertionFailure)
+  })
+
+  it('should error when not given an object', () => {
+    expect(() => menu([])).toThrow('invalid verbs:')
+    expect(() => menu()).toThrow('invalid verbs:')
+  })
+})
+
+describe('creating with messages', () => {
+  const foo = lambda(() => msg({ scalar: 123 }))
+  const bar = lambda(() => msg({ scalar: 456 }))
+  it('should allow construction with lambdas', () => {
+    const m = menu({ foo, bar })
+    expect(m).toBeInstanceOf(Msg)
+  })
+})
